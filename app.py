d26b5785068122e7093f4c0fa73be96065d20195
alex-pdl <apodlesny00@gmail.com>
2025-08-25 23:42:33 +0100

    Code formatting conforms to pep8 syntax.

--- app.py
+++ app.py
@@ -5,20 +5,22 @@ from flask_socketio import SocketIO, emit
 from utils.database_utils import (chats_retrieval, chat_creation,
                                   password_check, register, username_check,
                                   retrieve_privatekey, retrieve_public_key,
-                                  retrieve_chatid, determine_column, 
-                                  create_message, retrieve_messages, 
+                                  retrieve_chatid, determine_column,
+                                  create_message, retrieve_messages,
                                   create_database, retrieve_user_id)
 
-from crypto_algorithms import (generate_prime, key_gen, hash_password, 
+from crypto_algorithms import (generate_prime, key_gen, hash_password,
                                sym_encryption, sym_decryption,
                                rsa_encrypt, rsa_decrypt)
 
-from utils.input_utils import (get_salt, get_iterations, get_key_size, 
-                               get_secret_key, special_char_checker, 
+from utils.input_utils import (get_salt, get_iterations, get_key_size,
+                               get_secret_key, special_char_checker,
                                ascii_checker, string_to_tuple)
 
-# The program requires certain parameters to run, I have given the user 
+# The program requires certain parameters to run, I have given the user
 # the option to generate these paramters upon first startup
+
+
 def write_settings():
     salt = get_salt()
     iterations = get_iterations()
@@ -34,7 +36,7 @@ def write_settings():
         f"Key Size: {key_size}\n"
         f"Secret Key: {secret_key}"
     )
-    
+
     settings = {"salt": salt,
                 "iterations": iterations,
                 "primenumber": prime_number,
@@ -45,64 +47,74 @@ def write_settings():
     with open("settings.json", "w") as json_file:
         json.dump(settings, json_file)
 
+
 def is_login_valid(username, password):
     # Hashes password submitted
-    password_hash = hash_password(password,salt,iterations,prime_number)
-    
+    password_hash = hash_password(password, salt, iterations, prime_number)
+
     return password_check(username, password_hash)
-    
+
+
 def is_register_valid(username, password):
     if password == password.lower():
-        raise ValueError\
-            ("Password must include at least one uppercase letter.")
+        raise ValueError(
+            "Password must include at least one uppercase letter.")
 
     special_chars = special_char_checker(username)
     if len(special_chars) != 0:
-        raise ValueError\
-        (f"Username cannot include special characters such as: {special_chars}")
+        raise ValueError(
+            f"Username cannot include special characters such as: {special_chars}")
 
     ascii_chars = ascii_checker(password)
     if len(ascii_chars) != 0:
-        raise ValueError\
-            (f"Password contains invalid characters: {ascii_chars}")
+        raise ValueError(
+            f"Password contains invalid characters: {ascii_chars}")
 
     if username_check(username):
         raise ValueError("Username is already taken.")
-    
+
+
 def create_user(username, password):
     # Generates a public and private key for the user
     public_key, private_key = key_gen(keysize)
     # Encrypts the private key with the users password
     private_key = sym_encryption(str(private_key), password, 0, iterations)
     # Hashes the password
-    password_hash = hash_password(password,salt,iterations,prime_number)
-    
+    password_hash = hash_password(password, salt, iterations, prime_number)
+
     register(username, password_hash, str(public_key), private_key)
+
 
 def create_session(username, password):
     session['username'] = username
     session['usr_id'] = retrieve_user_id(username)
-    
+
     encrypted_private_key = retrieve_privatekey(username)
-    decrypted_private_key = sym_decryption(encrypted_private_key, password,0,iterations)
+    decrypted_private_key = sym_decryption(
+        encrypted_private_key, password, 0, iterations)
     session['private_key'] = decrypted_private_key
+
 
 def store_message(chatid, msg_contents, sender_username, receiver_username):
     sender_public_key = string_to_tuple(retrieve_public_key(sender_username))
-    receiver_public_key = string_to_tuple(retrieve_public_key(receiver_username))
+    receiver_public_key = string_to_tuple(
+        retrieve_public_key(receiver_username))
 
-    # Create two versions of the message, 1 encrypted using the senders info, 
+    # Create two versions of the message, 1 encrypted using the senders info,
     # and the other using the recievers info
     sender_message = str(rsa_encrypt(msg_contents, sender_public_key))
     receiver_message = str(rsa_encrypt(msg_contents, receiver_public_key))
-    
+
     # Determine which column (contents_1 or contents_2) the sender is a part of
     which_contents = determine_column(session["username"], chatid)
 
     if which_contents == "contents_1":
-        create_message(sender_username, chatid, sender_message, receiver_message)
+        create_message(sender_username, chatid,
+                       sender_message, receiver_message)
     elif which_contents == "contents_2":
-        create_message(sender_username, chatid, receiver_message, sender_message)
+        create_message(sender_username, chatid,
+                       receiver_message, sender_message)
+
 
 def decrypt_format_msgs(msg_info, encrypted_message_contents, private_key):
     messages = []
@@ -113,21 +125,25 @@ def decrypt_format_msgs(msg_info, encrypted_message_contents, private_key):
         # Get each message (encrypted)
         encrypted_contents = encrypted_message_contents[i][0]
         # Format the sender and timestamp & append to messages
-        formatted_info = str(info).replace("(", "").replace(")", "").replace("'", "")
+        formatted_info = str(info).replace(
+            "(", "").replace(")", "").replace("'", "")
         messages.append(str(formatted_info))
         # Format the message contents and decrypt
         # Then add to messages list
-        formatted_decrypted_message = ": " + rsa_decrypt(encrypted_contents, private_key)
+        formatted_decrypted_message = ": " + \
+            rsa_decrypt(encrypted_contents, private_key)
         messages[i] += formatted_decrypted_message
 
     return messages
+
 
 # Beginning of the website code
 app = Flask(__name__)
 socketio = SocketIO(app)
 # A standard homepage which redirects to the login page
-# If a user already has session data stored in cookies, 
+# If a user already has session data stored in cookies,
 # they will automatically be redirected to the chats page
+
 
 @app.route('/')
 def homepage():
@@ -137,39 +153,42 @@ def homepage():
     return redirect(url_for('login'))
 
 # The login page
-@app.route('/login', methods = ['GET','POST'])
+
+
+@app.route('/login', methods=['GET', 'POST'])
 def login():
-    if 'usr_id' in session: # Checks if user isn't already logged in
-        return redirect(url_for('chats'))
-    
+    if 'usr_id' in session:  # Checks if user isn't already logged in
+        return redirect(url_for('chats', ))
+
     if request.method == "POST":
         # Gets data submitted
         username = request.form.get('username')
         password = request.form.get('password')
 
-        if not is_login_valid(username,password):
+        if not is_login_valid(username, password):
             error = "You have not entered in the correct information. Please try again."
-            return render_template("login.html",error=error)
+            return render_template("login.html", error=error)
 
         create_session(username, password)
 
         # If user entered in correct info store the username, user id and private key in cookies
         return redirect(url_for('chats'))
-    
+
     return render_template("login.html")
 
-@app.route('/registration', methods = ['GET','POST'])
+
+@app.route('/registration', methods=['GET', 'POST'])
 def registration():
     # Submitting info (loggin in) uses POST, whereas loading the page uses GET
     if request.method == "POST":
         username = request.form.get('username')
         password = request.form.get('password')
-        
+
         try:
             is_register_valid(username, password)
         except ValueError as e:
-            return render_template("registration.html", error = e)
-        
+            return render_template("registration.html", error=e)
+
         create_user(username, password)
 
         create_session(username, password)
@@ -179,10 +198,11 @@ def registration():
 
     return render_template("registration.html")
 
-@app.route('/chats', methods = ['GET','POST'])
+
+@app.route('/chats', methods=['GET', 'POST'])
 def chats():
     # Checks if user isn't already logged in
-    if 'usr_id' not in session: 
+    if 'usr_id' not in session:
         return redirect(url_for('login'))
 
     username = session['username']
@@ -193,13 +213,14 @@ def chats():
 
     names = chats_retrieval(username)
 
-    return render_template("chats.html", user_name = str(username), names = names)
+    return render_template("chats.html", user_name=str(username), names=names)
 
-@app.route('/message',methods = ['GET', 'POST'])
+
+@app.route('/message', methods=['GET', 'POST'])
 def message():
     if 'usr_id' not in session:
         return redirect(url_for('login'))
-    
+
     username = session['username']
     selected_name = request.args.get('selected_name')
     chatid = retrieve_chatid(username, selected_name)
@@ -212,18 +233,19 @@ def message():
             return redirect(url_for('chats'))
         else:
             message = request.form.get('message')
-            store_message(chatid, message, username, selected_name)      
-    
+            store_message(chatid, message, username, selected_name)
+
     private_key = string_to_tuple(session['private_key'])
     column = determine_column(username, chatid)
     msg_info, encrypted_msg_contents = retrieve_messages(chatid, column)
-    
+
     msgs = decrypt_format_msgs(msg_info, encrypted_msg_contents, private_key)
-    
+
     if len(msgs) == 0:
         msgs = ["It appears you don't have any chats with this person. Say hi!"]
-    
-    return render_template('message.html', selected_user = selected_name, messages = msgs)
+
+    return render_template('message.html', selected_user=selected_name, messages=msgs)
+
 
 @socketio.on('add_chat')
 def add_chat(name):
@@ -236,7 +258,8 @@ def add_chat(name):
         socketio.emit('display_error', str(error))
         return
 
-    socketio.emit('add_chat_btn', name)
+    socketio.emit('add_chat_btn', name, namespace=username)
+
 
 if __name__ == "__main__":
     while True:
@@ -252,15 +275,17 @@ if __name__ == "__main__":
             keysize = data["key_size"]
             secret_key = data["secret_key"]
             break
-        except (FileNotFoundError, NameError): # If the settings.json file doesn't exist
+        except (FileNotFoundError, NameError):  # If the settings.json file doesn't exist
             print("Settings.json not detected. \nGenerating Settings.\n")
-            
+
             write_settings()
-            
+
             print("\nSettings configuration completed. The server will now start.\n")
-    
-    create_database() # Initialise database upon running the program
-    
-    app.config['SECRET_KEY'] = f'{secret_key}' # Use secret key specified in settings.json file
-    app.config['SESSION_COOKIE_HTTPONLY'] = True # Ensures that cookies are only accessible through HTTP(S) requests and cannot be accessed by any JavaScript
+
+    create_database()  # Initialise database upon running the program
+
+    # Use secret key specified in settings.json file
+    app.config['SECRET_KEY'] = f'{secret_key}'
+    # Ensures that cookies are only accessible through HTTP(S) requests and cannot be accessed by any JavaScript
+    app.config['SESSION_COOKIE_HTTPONLY'] = True
     socketio.run(app, debug=True)
